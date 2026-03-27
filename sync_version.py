feb069efbabe899a4d579ee0c02ff90de0017e27
Ir. Oskar P.W. Wols - Wols CA <oskar.wols@wolsca.com>
2026-03-27 16:28:27 +0100

    version 1.2.9, reverse version.yaml correction

--- sync_version.py
+++ sync_version.py
@@ -1,19 +1,14 @@
 import yaml
 
-# Copy version from config/version.yaml to config.yaml
-with open('config/version.yaml') as f:
-    version_data = yaml.safe_load(f) or {}
-
 try:
     with open('wols_ca_uploader/config.yaml') as f:
         config_data = yaml.safe_load(f) or {}
 except FileNotFoundError:
     config_data = {}
+f.close()
 
-if 'version' in config_data:
-    version_data['version'] = config_data['version']
-else:
-    print("Warning: 'version' not found in wols_ca_uploader/config.yaml")
+printString = "Version: " + str(config_data.get('version'));
+print(printString)
 
-with open('config/version.yaml', 'w') as f:
-    yaml.safe_dump(version_data, f)
+with open('wols_ca_uploader/config/version.yaml', 'w') as f:
+   f.write(printString)
